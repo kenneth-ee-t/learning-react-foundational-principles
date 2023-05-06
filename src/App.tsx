eb3b8345b15265af763732452dc8215b15822ddc
Kenneth Ewonde <Kennethewonde@takeo.ai>
2023-05-06 17:00:47 -0500

    Create list item

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import ListGroup from './component/ListGroup';
 function App() {
-  return <ul className="list-group">
-  <li className="list-group-item disabled" aria-disabled="true">A disabled item</li>
-  <li className="list-group-item">A second item</li>
-  <li className="list-group-item">A third item</li>
-  <li className="list-group-item">A fourth item</li>
-  <li className="list-group-item">And a fifth one</li>
-</ul>
+  const items = ['NY', 'OK', 'OH', 'TX'];
+
+  return (
+    <>
+      <h1>List of States</h1>
+      <ul className='list-group'>
+        {items.map((item) => (
+          <li key = {item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
